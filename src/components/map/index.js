952b3c4a75222dd719d76fcd12b179ca086289fb
Alex Hooper <Alex.Hooper@telegraph.co.uk>
2019-07-09 12:14:48 +0100

    marker and control ui

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import ReactMapboxGl, { Layer, Feature, map } from 'react-mapbox-gl';
 import Poi from './subcomponents/poi';
+import shareIcon from '../../assets/ui/share.svg';
+import hamburger from '../../assets/ui/hamburger.svg';
 import './style.scss';
 
 const Map = ReactMapboxGl({
@@ -24,8 +26,10 @@ export default function MapContainer({ data, activePoint, activePointSetter }) {
   return (
     <div className="map">
       <span className={`map__prompt ${activePoint !== -1 ? 'hidden' : ''}`}>Select a map pin below</span>
+      <button className="map__button map__button--share"><img src={hamburger} alt="menu" /></button>
+      <button className="map__button map__button--menu"><img src={shareIcon} alt="share" /></button>
       <Map
-        style="mapbox://styles/sparkdigitaldesign/cinrc6fmy00ckc7m5v3ktd8v4"
+        style="mapbox://styles/sparkdigitaldesign/cjxuni5kp0g901crv3a81b3db"
         containerStyle={{
           height: "100%",
           width: "100%"
--- src/components/ui/header/index.js
+++ src/components/ui/header/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import telegraphLogo from '../../../assets/logos/telegraph_logo.svg';
+import tourismIrelandLogo from '../../../assets/logos/tourism_ireland_logo.svg';
 import './style.scss';
 
 export default function Header() {
   return (
     <header>
-      Header
+      <div className="telegraph-logo">
+        <img src={telegraphLogo} alt="The Telegraph" />
+      </div>
+      <div className="sponsor-logo">
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <img src={tourismIrelandLogo} alt="Tourism Ireland" />
+        </a>
+      </div>
     </header>
   );
 }
