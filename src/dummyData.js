310c83af96f3ab0c58fe1a9c89ffc57c5aba3765
Daniel Cherino <daniel.cherino@telegraph.co.uk>
2019-07-09 15:50:46 +0100

    Leona data updated

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,58 +1,137 @@
 export const DummyData = [
   {
     name: 'Lough Erne',
-    address: '193 Lough Shore Rd, Ross Inner, Enniskillen BT93 7ED',
-    description: 'Lough Erne is one of Northern Ireland’s newer venues and offers stunning views as well as a top-class course that I know from experience is always in great shape. The closing stretch of 16-17-18 is especially good, the beautiful lough in play on all three holes. In any kind of wind they are enough to test even the best players. I know I have always been forced to hit some of my better shots just to make pars. The hotel, too, is lovely. For any length of golfing break it is just about the perfect place to stay.',
+    location: 'Enniskillen, Co Fermanagh',
+    description: 'Lough Erne is one of Northern Ireland’s newer venues and offers stunning views as well as a top-class course that I know from experience is always in great shape. The closing stretch of 16-17-18 is especially good, the beautiful lough in play on all three holes. In any kind of wind they are enough to test even the best players. I know I have always been forced to hit some of my better shots just to make pars. The hotel is lovely too. For any length of golfing break it is just about the perfect place to stay.',
     lat: 54.3979406,
     long: -7.6956315,
-    golfer: 'leona'
+    golfer: 'leona',
+    image: 'lough_erne.jpg'
   },
   {
     name: 'Malone',
-    address: '240 Upper Malone Rd, Dunmurry, Belfast BT17 9GA',
-    description: 'Lough Erne is one of Northern Ireland’s newer venues and offers stunning views as well as a top-class course that I know from experience is always in great shape. The closing stretch of 16-17-18 is especially good, the beautiful lough in play on all three holes. In any kind of wind they are enough to test even the best players. I know I have always been forced to hit some of my better shots just to make pars. The hotel, too, is lovely. For any length of golfing break it is just about the perfect place to stay.',
+    location: 'Dunmurry, Belfast',
+    description: 'This is a great track, one I know very well. As an amateur, I played here in many training tournaments, as well as the 2015 Vagliano Trophy between Great Britain & Ireland and the Continent of Europe, and a British Open Amateur Strokeplay Championship. The greens at Malone are renowned as some of the best in the country. And the whole course is always in terrific shape, the mix of holes in and out of the trees providing a varied test. You’ll likely need every club in your bag. Just one of my favourite places to play golf, whether in competition or socially.',
     lat: 54.5386906,
     long: -5.9806315,
     golfer: 'leona',
+    image: 'malone.jpg'
   },
   {
     name: 'Belvoir Park',
-    address: '73 Church Rd, Castlereagh, Belfast BT8 7AN',
-    description: 'Lough Erne is one of Northern Ireland’s newer venues and offers stunning views as well as a top-class course that I know from experience is always in great shape. The closing stretch of 16-17-18 is especially good, the beautiful lough in play on all three holes. In any kind of wind they are enough to test even the best players. I know I have always been forced to hit some of my better shots just to make pars. The hotel, too, is lovely. For any length of golfing break it is just about the perfect place to stay.',
+    location: 'Newtownbreda, Belfast',
+    description: 'One of my favourite inland courses in Northern Ireland, a place where I have so many fond memories of playing. I particularly love the elevation changes that are such a feature of many holes. But don’t be fooled by the location and the scenery. A few of the holes can get a bit “tight” off the tee. So straight drives are a must, especially when the rough is up. The trees are lovely to look at but not so nice to play out of. But this is a very fair course. Hit good shots and you will invariably be well rewarded.',
     lat: 54.5614406,
     long: -5.9163815,
     golfer: 'leona',
+    image: 'belvoir_park.jpg'
   },
   {
     name: 'Castlerock',
-    address: '65 Circular Rd, Castlerock, Coleraine BT51 4TJ',
-    description: 'Lough Erne is one of Northern Ireland’s newer venues and offers stunning views as well as a top-class course that I know from experience is always in great shape. The closing stretch of 16-17-18 is especially good, the beautiful lough in play on all three holes. In any kind of wind they are enough to test even the best players. I know I have always been forced to hit some of my better shots just to make pars. The hotel, too, is lovely. For any length of golfing break it is just about the perfect place to stay.',
+    location: 'Co Londonderry',
+    description: 'Because of its proximity to Portrush, Castlerock, like Portstewart, never seems to get the credit as it deserves. But believe me, it merits your attention. This is a terrific links, one that is nearly always firm and bouncy in summer, which only adds to the questions asked of your game. This isn’t the longest course, but driving is important. Accurate tee shots will set you up nicely for the approaches into what are tricky greens. I have never had anything but fun playing and practising at Castlerock, which is as good a recommendation as I can give any course.',
     lat: 55.1658155,
     long: -6.7868815,
-    golfer: 'leona'
+    golfer: 'leona',
+    image: 'castlerock.jpg'
   },
   {
     name: 'Ardglass',
-    address: 'Castle Pl, Ardglass, Downpatrick BT30 7TP',
-    description: 'Lough Erne is one of Northern Ireland’s newer venues and offers stunning views as well as a top-class course that I know from experience is always in great shape. The closing stretch of 16-17-18 is especially good, the beautiful lough in play on all three holes. In any kind of wind they are enough to test even the best players. I know I have always been forced to hit some of my better shots just to make pars. The hotel, too, is lovely. For any length of golfing break it is just about the perfect place to stay.',
+    location: 'Co Down',
+    description: 'This is yet another lovely links that is perhaps a little underrated due to being not too far from Royal County Down. Home to the oldest clubhouse on the island of Ireland – it dates back to 1405 – Ardglass is well worthy of greater attention. Certainly, the enchanting scenery is enough to distract you from any task in hand. The views across to the Isle of Man and south to the mountains of Mourne are stunning. And the golf course lives up to its setting. The great old golf writer, Bernard Darwin, called it “pleasurable excitement”. I like that. I think you will too.',
     lat: 54.2584406,
     long: -5.6080065,
-    golfer: 'padraig',
+    golfer: 'leona',
+    image: 'ardglass.jpg'
   },
   {
     name: 'Slieve Russell',
-    address: 'Cranaghan, Ballyconnell, Co. Cavan, Ireland',
-    description: 'Lough Erne is one of Northern Ireland’s newer venues and offers stunning views as well as a top-class course that I know from experience is always in great shape. The closing stretch of 16-17-18 is especially good, the beautiful lough in play on all three holes. In any kind of wind they are enough to test even the best players. I know I have always been forced to hit some of my better shots just to make pars. The hotel, too, is lovely. For any length of golfing break it is just about the perfect place to stay.',
+    location: 'Ballyconnell, Co Cavan',
+    description: 'Forgive my natural bias, but my home course has it all when it comes to golf. It has trees, it has water in play on a few holes, it has tricky, sloping greens, it has changes in elevation, it has some wonderful par-threes that will stick in your memory. And the par- fives are tempting enough for just about any standard of golfer. It is just a wonderful place to play, one I have appreciated more as I have spent more time away from home. I especially miss the pure putting surfaces and the par-three course. Great fun.',
     lat: 54.0966906,
     long: -7.5595065,
-    golfer: 'padraig',
+    golfer: 'leona',
+    image: 'slieve_russell.jpg'
   },
   {
     name: 'Headfort',
-    address: 'Kilmainham (Headfort), Kells, Co. Meath, Ireland',
-    description: 'Lough Erne is one of Northern Ireland’s newer venues and offers stunning views as well as a top-class course that I know from experience is always in great shape. The closing stretch of 16-17-18 is especially good, the beautiful lough in play on all three holes. In any kind of wind they are enough to test even the best players. I know I have always been forced to hit some of my better shots just to make pars. The hotel, too, is lovely. For any length of golfing break it is just about the perfect place to stay.',
+    location: 'Kells, Co Meath',
+    description: 'This is a place I like and have visited often, especially if I am preparing for a tournament on a course where I know straight driving is going to be required. Headfort has many tree-lined holes that will test your accuracy from the tee. When the leaves are changing colour on those trees, the effect is spectacular. It is just a good all-round test of golf, with a nice variety of lies and stances to test your versatility. I have never been there when it has been in anything less than superb shape. Just a treat to play.',
     lat: 53.7251907,
     long: -6.8623815,
-    golfer: 'padraig',
+    golfer: 'leona',
+    image: 'headfort.jpg'
   },
+  {
+    name: 'Portmarnock',
+    location: 'Dublin',
+    description: 'Located just outside Dublin, this is one of my favourite links courses. This may sound strange, but I am especially fond of visiting in winter. It is a wonderful place to practise every aspect of your game. Many of the greens have some subtle “run-offs” that will test your chipping technique. The pot bunkers are always challenging. And playing in the wind asks questions of your ability to control the trajectory of your shots. The par-threes will stick in your mind’s eye too. The 17th is especially memorable. All in all, just a place I love to be.',
+    lat: 53.4295657,
+    long: -6.1282565,
+    golfer: 'leona',
+    image: 'portmarnock.jpg'
+  },
+  {
+    name: 'Lahinch',
+    location: 'Co Clare',
+    description: 'Home to the annual South of Ireland Championship and universally hailed as one of the nation’s best links, the Old Course at Lahinch – often called the “St. Andrews of Ireland” – was a stunning venue for the recent Irish Open. There are so many good and great holes on Lahinch it is hard to pick out the best of the bunch. But for me the back nine is slightly stronger than the front. Which is not to say the first few holes are not tough enough. But the 11th and 12th holes down by the water are those you will remember most.',
+    lat: 52.9401907,
+    long: -9.3557565,
+    golfer: 'leona',
+    image: 'lahinch.jpg'
+  },
+  {
+    name: 'County Louth',
+    location: 'Baltray',
+    description: 'Forgive my bias, but Baltray, as this wonderful links is known, was the venue for my victory in the Irish Championship in 2012. It is a little different from the norm too, in that the sea is not actually in view that often. But the holes in and around the dunes more than make up for any sense of loss in that department. This is a course renowned also for its inherent fairness. Good shots will nearly always be rewarded – bad shots not so much. One last word of advice: avoid the pot bunkers off the tee at all costs.',
+    lat: 53.7383157,
+    long: -6.2638815,
+    golfer: 'leona',
+    image: 'county_louth.jpg'
+  },
+  {
+    name: 'Black Bush',
+    location: 'Thomastown, Co Meath',
+    description: 'Yet another beautiful parkland course in the Dublin area, Black Bush has three loops of nine holes that require a variety of shot making. I have always found it to be very enjoyable, the smallish greens and their surrounds are good test of approach play and chipping. I also like that almost every hole has a number of tees. So you can make each hole as difficult or as straightforward as you wish. Golf doesn’t have to be tough to be fun. Which is perhaps why Black Bush’s Open Week is one of the most popular in Ireland. They are doing something right.',
+    lat: 53.5275657,
+    long: -6.5295065,
+    golfer: 'leona',
+    image: ''
+  },
+  {
+    name: 'Knightsbrook',
+    location: 'Trim, Co Meath',
+    description: 'Another course I know well, having played in the Junior Solheim Cup here in 2011. Right away I could see that Knightsbrook – whose on-site hotel is ideal for a family holiday – is a really good course “made” for head-to-head matchplay, many of the holes offering a tempting mix of risk and reward. As tends to be the modern way, this is also a course that asks for superior ball striking, especially when attacking the subtly sloping putting surfaces. Although relatively forgiving off the tee, the second shots are invariably both long and testing.',
+    lat: 53.5485657,
+    long: -6.7666315,
+    golfer: 'leona',
+    image: 'knightsbrook.jpg'
+  },
+  {
+    name: 'County Sligo',
+    location: 'Rosses Point',
+    description: 'Home every spring to the prestigious West of Ireland Championship, Rosses Point is one of Ireland’s best courses, inland or seaside. Especially when the wind blows, it is a strong test for even the best players. There is no let-up either. After some really questioning driving holes in the middle of the round, the finishing holes are among the toughest and most memorable you’ll find just about anywhere. The dramatic uphill 17th will surely stick in your mind for ever. And the last, with its blind tee shot, is a wonderful way to finish a round on a wonderful course.',
+    lat: 54.3070656,
+    long: -8.5680065,
+    golfer: 'leona',
+    image: 'county_sligo.jpg'
+  },
+  {
+    name: 'Old Head',
+    location: 'Kinsale, Co Cork',
+    description: 'It is safe to say that there are not many courses in the world like Old Head. This spectacular and dramatic links occupies a jagged headland that juts two miles out into the Atlantic Ocean. As such, there is an “edge of the world” feel to many of the holes. Indeed, one or two of the more adventurous tees are no place for anyone suffering from vertigo. The hole that will stay with you the longest is perhaps the par-five 12th. Over-ambition off the tee can only lead to you reaching for a second ball. You have been warned.',
+    lat: 51.6088158,
+    long: -8.5370065,
+    golfer: 'leona',
+    image: 'old_head.jpg'
+  },
+  {
+    name: 'Ballybunion',
+    location: 'Co Kerry',
+    description: 'If you are at all into taking a breather during a round of golf to take in the views, the Old Course at Ballybunion is the place for you. The late, great American golf writer, Herbert Warren Wind, described the links as “nothing less than the finest seaside course I have ever seen”. High praise indeed from the man who christened “Amen Corner” at Augusta National. And it is hard to disagree with him. This is links golf at its best and wildest and, in places, most strategic. No golfing visit to Ireland is complete without a visit to Ballybunion.',
+    lat: 52.5110657,
+    long: -9.6731315,
+    golfer: 'leona',
+    image: 'ballybunion.jpg'
+  }
 ];
